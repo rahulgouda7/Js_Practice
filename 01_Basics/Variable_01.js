d323277821d3f754c76ff689af7062787bd386af
Rahul_Gouda <94516425+rahulgouda7@users.noreply.github.com>
2024-02-29 07:59:48 +0000

    variable

---
+++ 01_Basics/Variable_01.js
@@ -0,0 +1,7 @@
+const accountId = 144553
+let accountEmail = "rahul589@gmail.com"
+var accountPassword ="12345"
+ accountCity ="Berhampur"
+// accountId =2 // not allowed
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity]);
