c140709d06f41e128852478b432e30b7ed31b876
Rahul_Gouda <94516425+rahulgouda7@users.noreply.github.com>
2024-02-29 09:22:38 +0000

    DataTypes

---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all js code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+console.log(3+3)
+console.log("Rahul")
+
+
+let name ="Rahul"
+let age = 22
+let isLoggedIn = false
+
+// number => 2 to power 53
+//bigint
+//string => ""
+//boolean => True/false
+//null => standalone value
+//undefined => 
+//symbol => unique
+
+
+//object
+console.log(typeof "Rahul");
