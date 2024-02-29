61b4f85c1d3db8422763990a2ca933144d87b812
Rahul_Gouda <94516425+rahulgouda7@users.noreply.github.com>
2024-02-29 10:02:21 +0000

    operation

---
+++ 01_Basics/04_operation.js
@@ -0,0 +1,24 @@
+let value =3
+let negvalue = -value
+console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 ="Hello"
+let str2 =" Rahul"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2")
+console.log("1" + 2 + 2);
+console.log(1+2+ "2");
+
+console.log(true);
+console.log(+true);
+console.log(+"");
+
