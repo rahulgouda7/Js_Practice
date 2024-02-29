f88a7ed0d8ca3dab2ed9fa4f4e277db1d7bd1121
Rahul_Gouda <94516425+rahulgouda7@users.noreply.github.com>
2024-02-29 09:45:46 +0000

    conversion of datatypes

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,19 @@
+let score ="33abc"
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+let isLoggedIn=0
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+//1 => true; 0 => false
+// "" => false
+//"Rahul"=>true
+
+let someNumber = 22
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber);
