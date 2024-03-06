3af4acd6282f983827da41adeef3f56937341f59
Rahul_Gouda <94516425+rahulgouda7@users.noreply.github.com>
2024-03-06 07:49:09 +0000

    Introduction on functions

---
+++ 03_Basics/01_functions.js
@@ -0,0 +1,35 @@
+
+
+// function sayMyName(){
+//     console.log("R");
+//     console.log("A");
+//     console.log("H");
+//     console.log("U");
+//     console.log("L");
+// }
+// sayMyName
+
+// function addTwoNumbers(number1, number2){
+//    console.log( number1+number2);
+// }
+// addTwoNumbers(3,4)
+
+function addTwoNumbers(number1, number2){
+    // let result =  number1 + number2
+    // return result
+    return number1 + number2
+}
+const result = addTwoNumbers(3,5)
+// console.log("Result: ", result);
+
+
+
+function loginUserMessage(username = "sam" ){
+    if(!username){
+        console.log("please enter a user name");
+        return
+    }
+    return `${username} just logged in`
+}
+console.log(loginUserMessage("Rahul"));
+// console.log(loginUserMessage());
