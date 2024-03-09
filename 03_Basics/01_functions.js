7d0e4ec60991477e1b7581e6b3c3fe4d19d52f35
Rahul_Gouda <94516425+rahulgouda7@users.noreply.github.com>
2024-03-09 06:18:57 +0000

    function

--- 03_Basics/01_functions.js
+++ 03_Basics/01_functions.js
@@ -31,5 +31,30 @@ function loginUserMessage(username = "sam" ){
     }
     return `${username} just logged in`
 }
-console.log(loginUserMessage("Rahul"));
+// console.log(loginUserMessage("Rahul"));
 // console.log(loginUserMessage());
+
+
+function calculateCartPrice(val1, val2,...num1){
+    return num1
+}
+// console.log(calculateCartPrice(200, 400, 500,2000));
+
+const user ={
+    username: "Rahul",
+    price: 199
+}
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}  
+// handleObject(user)
+handleObject({
+    username: "Rahul",
+    price: 199
+})
+
+const myNewArray = [200,400,600]
+ function returnSecondValue(getArray){
+    return getArray[1]
+ }
+ console.log(returnSecondValue(myNewArray));
