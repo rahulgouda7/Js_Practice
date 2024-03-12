16050d4ef8adf2dc1d1ac70079e26d3f93889873
Rahul_Gouda <94516425+rahulgouda7@users.noreply.github.com>
2024-03-12 04:42:38 +0000

    Scope

---
+++ 04_Control_flow/01_if.js
@@ -0,0 +1,13 @@
+// if 
+// const isUserloggedIn = true
+// if (isUserloggedIn){
+
+// }
+
+
+const score = 200
+
+if (score > 100){
+    const power = "fly"
+    console.log(`User power: ${power}`);
+}
