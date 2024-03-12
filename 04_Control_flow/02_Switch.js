f96bf2ddfaa956e96d2b7a33732fe293ff6a5a25
Rahul_Gouda <94516425+rahulgouda7@users.noreply.github.com>
2024-03-12 05:00:22 +0000

    Switch_Case

---
+++ 04_Control_flow/02_Switch.js
@@ -0,0 +1,52 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 3
+switch (month) {
+    case 1:
+        console.log("jan");
+        break;
+    case 2:
+        console.log("Feb");
+        break;
+    case 3:
+        console.log("Mar");
+        break;
+    case 4:
+        console.log("Apr");
+        break;
+    case 5:
+        console.log("May");
+        break;
+    case 6:
+        console.log("jun");
+        break;
+    case 7:
+        console.log("jul");
+        break;
+    case 8:
+        console.log("Aug");
+        break;
+    case 9:
+        console.log("sep");
+        break;
+    case 10:
+        console.log("Oct");
+        break;
+    case 11:
+        console.log("Nov");
+        break;
+    case 12:
+        console.log("Dec");
+        break;
+
+    default:
+        console.log("Enter a valid month number");
+        break;
+}
