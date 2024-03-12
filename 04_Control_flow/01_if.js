53cf494a2c3c139c7fbfb6058923bb34dbbafa9b
Rahul_Gouda <94516425+rahulgouda7@users.noreply.github.com>
2024-03-12 04:52:54 +0000

    Scopes

--- 04_Control_flow/01_if.js
+++ 04_Control_flow/01_if.js
@@ -5,9 +5,34 @@
 // }
 
 
-const score = 200
+// const score = 200
 
-if (score > 100){
-    const power = "fly"
-    console.log(`User power: ${power}`);
+// if (score > 100){
+//     const power = "fly"
+//     console.log(`User power: ${power}`);
+// }
+
+// const balance = 1000
+// // if (balance > 500) console.log("test"),
+// // console.log("test2");
+
+// if (balance < 500){
+//     console.log("less than");
+// }else if (balance < 750){
+//     console.log("less than 750");
+// }else if (balance < 900){
+//     console.log("less than 750");
+// }else{
+//     console.log("less than 1200");
+// }
+
+const isUserloggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+if(isUserloggedIn && debitCard){
+    console.log("Allow to buy course");
+}
+if(loggedInFromGoogle || loggedInFromEmail){
+    console.log("User logged in");
 }
