f1f4df636a70694693c0d64ebddb1f2b5229a2f3
Rahul_Gouda <94516425+rahulgouda7@users.noreply.github.com>
2024-03-15 06:37:38 +0000

    Truthy

---
+++ 04_Control_flow/03_Truthy.js
@@ -0,0 +1,39 @@
+const userEmail = "rahul@gmail.com"
+
+if (userEmail){
+    console.log("Got user email");
+}else{
+    console.log("Dont have user email");
+}
+
+
+// falsy value
+// false,0,-0, BigInt 0n, "", null, undefined, NaN
+
+//Truthy value
+// "0", 'false', " ", [], {}, function(){}
+
+// if (userEmail.length === 0){
+//     console.log("Array is empty");
+// }
+
+// const emptyObj = {}
+// if(Object.keys(emptyObj).length === 0){
+//     console.log("Object is empty");
+// }
+
+
+// Nullish Coalescing Operator (??): null uderfined
+
+// let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 15
+
+// console.log(val1);
+
+// Terniary Operator
+
+// condition ? true:false
+const iceTeaPrice = 100
+iceTeaPrice >= 80 ? console.log("less than 80") : console.log("more than 80");
