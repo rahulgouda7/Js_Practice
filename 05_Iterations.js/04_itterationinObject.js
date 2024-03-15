b396afbcdad556f1e19ffc6033ae470775b2d910
Rahul_Gouda <94516425+rahulgouda7@users.noreply.github.com>
2024-03-15 08:04:53 +0000

    forof, forin

---
+++ 05_Iterations.js/04_itterationinObject.js
@@ -0,0 +1,19 @@
+// const myObject = {
+//     js: 'Javascript',
+//     cpp: 'c++',
+//     rb: "ruby",
+//     swift: "swift by apple"
+// }
+// for (const key in myObject) {
+//     console.log(` ${key} shortcut is for ${myObject[key]}`);
+// }
+
+// const map = new Map()
+// map.set('IN', "INDIA")
+// map.set('US',"USA")
+// map.set('Fr', "France")
+// // console.log(map);
+
+// for (const [key, value] in map) {
+//     console.log(key, ':-', value);
+// }
