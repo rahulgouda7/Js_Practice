25f024cda44870b125fe51b3086560bdebd5c322
Rahul_Gouda <94516425+rahulgouda7@users.noreply.github.com>
2024-03-16 07:29:12 +0000

    filter

---
+++ 05_Iterations.js/06.js
@@ -0,0 +1,13 @@
+// const coding = ["js", "Py","c++","ruby" ]
+// const values =coding.forEach ( (item) =>{
+//     console.log(item);
+// })
+// console.log(values);
+
+
+const myNums = [1,2,3,4,5,6,7,8,9,10]
+const newNums =myNums.filter( (num) =>{
+    return num>4
+})
+console.log(newNums);
+
